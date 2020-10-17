1bebf09fafdf621ad5d0c34668520438d0de4bac
Omer <omrebres@gmail.com>
2020-10-17 17:03:16 +0300

    Changes

--- README.md
+++ README.md
@@ -1,28 +1,31 @@
 ## Digital Signature
 **Overview:**  
-Once the user provides a file, the program would generate a hash key out of the file using the *Folding Method*.  
-The hash key would then be encrypted using a private key, which is generated through the *RSA algorithm*.  
+Once the user provides a file, the program would use a hash functionon the file, which uses the *Folding Method*.  
+The hash function would then be encrypted using a private key, which is generated through the *RSA algorithm*.  
 Finally, in order to verify the file's digital signature, the encrypted hash would be decrypted using public key, which is also generated through the *RSA algorithm*, and the decrypted hash would be compared with the original hash.  
 If the two are equal, the digitial signature process is valid and complete.  
-
+  
+The public key should go inside a file named rsa.public.  
+The signature should go inside a file named signature.enc. 
+Both files should be located inside the same directory.  
+  
 **Usage:**  
 
 Compiling:  
 `make build`  
+    
+Validating the Digital Signature:  
+`make validate_signature INPUT="yourFile"`  
   
-Generating Public & Private Keys:  
+Generating your own Public & Private Keys:  
 `make generate_keys`  
   
 Signing a file:  
 `make sign INPUT="yourFile"`  
   
-Validating the Digital Signature:  
-`make validate_signature INPUT="yourFile"`  
-  
 Cleanup:  
 `make clean`  
   
-
 By:  
 Shalev Abadi  
 Omer Bresinski
--- rsa.c
+++ rsa.c
@@ -1,82 +1,88 @@
-#include<stdio.h> 
-#include<math.h> 
+#include <stdio.h>
+#include <math.h>
 
-int exponentMod(int A, int B, int C) 
-{ 
-	// Base cases 
-	if (A == 0) 
-		return 0; 
-	if (B == 0) 
-		return 1; 
-
-	// If B is even 
-	long y; 
-	if (B % 2 == 0) { 
-		y = exponentMod(A, B / 2, C); 
-		y = (y * y) % C; 
-	} 
-
-	// If B is odd 
-	else { 
-		y = A % C; 
-		y = (y * exponentMod(A, B - 1, C) % C) % C; 
-	} 
-
-	return (int)((y + C) % C); 
-} 
-
-int modInverse(int a, int m) 
-{ 
-    a = a%m; 
-    for (int x=1; x<m; x++) 
-       if ((a*x) % m == 1) 
-          return x; 
-} 
-
-// Returns gcd of a and b 
-int gcd(int a, int h) 
-{ 
-    int temp; 
-    while (1) 
-    { 
-        temp = a%h; 
-        if (temp == 0) 
-          return h; 
-        a = h; 
-        h = temp; 
-    } 
-} 
-
-int isPrime(int n)  
-{  
-    // Corner cases  
-    if (n <= 1)  return 0;  
-    if (n <= 3)  return 1;  
-    
-    // This is checked so that we can skip   
-    // middle five numbers in below loop  
-    if (n%2 == 0 || n%3 == 0) return 0;  
-    
-    for (int i=5; i*i<=n; i=i+6)  
-        if (n%i == 0 || n%(i+2) == 0)  
-           return 0;  
-    
-    return 1;  
-}  
-
-int generateKeys(int p, int q, int* resN, int* publicK, int* privateK)
+int exponentMod(int A, int B, int C)
 {
-    int n = p*q; 
-    int e = 2; 
-    int phi = (p-1)*(q-1); 
-    while (e < phi) 
-    { 
-        if (isPrime(e) && gcd(e, phi)==1) 
+    // Base cases
+    if (A == 0)
+        return 0;
+    if (B == 0)
+        return 1;
+
+    // If B is even
+    long y;
+    if (B % 2 == 0)
+    {
+        y = exponentMod(A, B / 2, C);
+        y = (y * y) % C;
+    }
+
+    // If B is odd
+    else
+    {
+        y = A % C;
+        y = (y * exponentMod(A, B - 1, C) % C) % C;
+    }
+
+    return (int)((y + C) % C);
+}
+
+int modInverse(int a, int m)
+{
+    a = a % m;
+    for (int x = 1; x < m; x++)
+        if ((a * x) % m == 1)
+            return x;
+}
+
+// Returns gcd of a and b
+int gcd(int a, int h)
+{
+    int temp;
+    while (1)
+    {
+        temp = a % h;
+        if (temp == 0)
+            return h;
+        a = h;
+        h = temp;
+    }
+}
+
+int isPrime(int n)
+{
+    // Corner cases
+    if (n <= 1)
+        return 0;
+    if (n <= 3)
+        return 1;
+
+    // This is checked so that we can skip
+    // middle five numbers in below loop
+    if (n % 2 == 0 || n % 3 == 0)
+        return 0;
+
+    for (int i = 5; i * i <= n; i = i + 6)
+        if (n % i == 0 || n % (i + 2) == 0)
+            return 0;
+
+    return 1;
+}
+
+int generateKeys(int p, int q, int *resN, int *publicK, int *privateK)
+{
+    int n = p * q;
+    int e = 2;
+    int phi = (p - 1) * (q - 1);
+    while (e < phi)
+    {
+        if (isPrime(e) && gcd(e, phi) == 1)
             break;
         else
-            e++; 
-    } 
-    if(e >= phi){
+            e++;
+    }
+    if (e >= phi)
+    {
         printf("Can't find E\n");
         return 0;
     }
@@ -95,9 +101,9 @@ int decrypt(int cipher, int privateK, int keyLength)
     return exponentMod(cipher, privateK, keyLength);
 }
 
-int encrypt(int plain, int publicK, int keyLength)
+int encrypt(int plain, int privateK, int keyLength)
 {
-    return exponentMod(plain, publicK, keyLength);
+    return exponentMod(plain, privateK, keyLength);
 }
 
 int isValidSignature(int hash, int publicK, int keyLength, int expectedCipher)
